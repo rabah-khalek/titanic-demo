efcdf50e4e02d50b5f723dc41e066d75342a2a03
Rabah <rabah.khalek@gmail.com>
2023-05-21 16:32:26 +0200

    added train.py

---
+++ train.py
@@ -0,0 +1,15 @@
+from giskard import demo
+
+# Train your model
+clf, df = demo.titanic()
+
+# Fill in the artifacts needed to run the giskard app
+artifacts = {"Model":{}, "Dataset":{}}
+
+artifacts["Model"]["model"]=clf
+artifacts["Model"]["model_type"]="classification"
+
+artifacts["Dataset"]["df"]=df
+artifacts["Dataset"]["target"]="Survived"
+
+return artifacts
